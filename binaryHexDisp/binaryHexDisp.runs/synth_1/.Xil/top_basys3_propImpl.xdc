b7518f49cd2fac12df2b2006a8bace1fb317dfa8
price-jamar <c26jamar.price@afacademy.af.edu>
2024-02-27 17:06:24 -0700

    Finished2

---
+++ binaryHexDisp/binaryHexDisp.runs/synth_1/.Xil/top_basys3_propImpl.xdc
@@ -0,0 +1,3 @@
+set_property SRC_FILE_INFO {cfile:C:/Users/Jamar.Price/Desktop/ece281-lab2/binaryHexDisp/binaryHexDisp.srcs/constrs_1/imports/hdl/Basys3_Master.xdc rfile:../../../binaryHexDisp.srcs/constrs_1/imports/hdl/Basys3_Master.xdc id:1} [current_design]
+set_property src_info {type:XDC file:1 line:82 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN W7 [get_ports {seg[0]}]
--- binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/xsim_webtalk.tcl
+++ binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/Users/Jamar.Price/OneDrive - afacademy.af.edu/ECE/ECE 281/Labs/Lab 2/lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/Jamar.Price/Desktop/ece281-lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Feb 27 16:16:08 2024" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Feb 27 17:05:43 2024" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "379b7622-22fc-4b38-8971-0fe62d6aee90" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "e193fef4ebeb47929d54c5d3e8614b20" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "9" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "11" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7276_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1932476351 -regid "" -xml C:/Users/Jamar.Price/OneDrive - afacademy.af.edu/ECE/ECE 281/Labs/Lab 2/lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Jamar.Price/OneDrive - afacademy.af.edu/ECE/ECE 281/Labs/Lab 2/lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Jamar.Price/OneDrive - afacademy.af.edu/ECE/ECE 281/Labs/Lab 2/lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7816_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 4125349408 -regid "" -xml C:/Users/Jamar.Price/Desktop/ece281-lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Jamar.Price/Desktop/ece281-lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Jamar.Price/Desktop/ece281-lab2/binaryHexDisp/binaryHexDisp.sim/sim_1/behav/xsim/xsim.dir/sevenSegDecoder_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
